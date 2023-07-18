94c6983ba5654b4a7da21fd7b4b84497e3e771fe
Mikhail Waddell <mikhail.waddell@gmail.com>
2023-07-18 15:12:42 -0500

    update forms

--- scripts/fleet.js
+++ scripts/fleet.js
@@ -1,3 +1,30 @@
+//Format Phone number
+function formatPhoneNumber(value) {
+    if (!value) return value;
+    const phoneNumber = value.replace(/[^\d]/g, '');
+    const phoneNumberLength = phoneNumber.length;
+    if (phoneNumberLength < 4) return phoneNumber;
+    if (phoneNumberLength < 7) {
+        return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(3)}`;
+    }
+    return `(${phoneNumber.slice(0, 3)}) ${phoneNumber.slice(
+        3,
+        6
+    )}-${phoneNumber.slice(6, 9)}`;
+}
+
+function phoneNumberFormatter() {
+    const inputField = document.getElementById('phone');
+    const formattedInputValue = formatPhoneNumber(inputField.value);
+    inputField.value = formattedInputValue;
+}
+
+//Update Select Vehicle Form
+function updateSelected(val) {
+    let select = document.querySelector('select');
+    console.log(select.value);
+    select.value = val;
+}
 //Set new date
 let curDate = document.getElementById('dates').getAttribute('value');
 date = new Date();
@@ -5,7 +32,7 @@ year = date.getFullYear();
 month = date.getMonth() + 1;
 day = date.getDate();
 curDate = month + "/" + day + "/" + year;
-console.log(curDate);
+// console.log(curDate);
 
 //Data Range Calender Picker
 $(function getDate() {
